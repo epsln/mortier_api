d207ce19627e061e4332f3b7c0a5f188cb624578
epsilon <martin.olivier1997@gmail.com>
2026-02-18 18:35:36 +0100

    more validation

--- src/mortier_api/main.py
+++ src/mortier_api/main.py
@@ -2,18 +2,19 @@ from fastapi import FastAPI, Response,  HTTPException
 from fastapi.middleware.gzip import GZipMiddleware
 
 from pydantic import BaseModel, Field, PositiveInt, PositiveFloat, NonNegativeInt, NonNegativeFloat
-from typing import Annotated, Tuple, Literal, Optional
+from typing import Annotated, Tuple, Literal, Optional, Union
 
 from mortier.tesselation import RegularTesselation, HyperbolicTesselation, PenroseTesselation
 from mortier.writer import SVGWriter 
+from mortier.writer.ornements import Ornements
 from mortier.enums import TileType, ParamType, HatchType, OrnementsType, TesselationType, RegularTesselationType
 from mortier.writer.hatching import Hatching
 from mortier.writer.ornements import Ornements
+from mortier.writer.hatching import Hatching
 from fastapi.middleware.cors import CORSMiddleware
 
 import json
 import random
-import time
 
 app = FastAPI()
 with open('data/database.json', 'r') as file:
@@ -23,20 +24,24 @@ TESS_IDS = list(js.keys())
 TESS_IDS.append("random")
 
 class RegularTessParameters(BaseModel):
+    type: Literal["regular"]
     tess_id: RegularTesselationType
 
 class HyperbolicTessParameters(BaseModel):
+    type: Literal["hyperbolic"]
     n_sides: PositiveInt 
     n_neigh: PositiveInt 
     depth: PositiveInt 
     refinements: int 
     half_plane: bool
 
+class PenroseTessParameters(BaseModel):
+    type: Literal["penrose"]
+    tile:  Literal[TileType.P2, TileType.P3]
+    depth: PositiveInt
+
 class AngleParametrisation(BaseModel):
     type: ParamType
-
-class PenroseTessParameters(BaseModel):
-    tile:  Literal[TileType.P2, TileType.P3]
 
 class OrnementsParameters(BaseModel):
     type:  OrnementsType
@@ -49,8 +54,9 @@ class HatchingParameters(BaseModel):
     angle: float
  
 class Params(BaseModel):
-    tess_type: TesselationType 
-    tess_id: RegularTesselationType 
+    tess_parameters: Union[RegularTessParameters, 
+                           HyperbolicTessParameters, 
+                           PenroseTessParameters] = Field(discriminator = "type")
     size: Annotated[
         Tuple[PositiveInt, PositiveInt],
         Field(
@@ -62,8 +68,8 @@ class Params(BaseModel):
     scale: PositiveInt = 70
     angle: float
     angle_parametrisation: Optional[ParamType] = None
-    ornements: Optional[OrnementsParameters] = None
-    hatching: Optional[HatchingParameters] = None
+    ornements: Optional[Ornements] = None
+    hatching: Optional[Hatching] = None
     color_line: Tuple[NonNegativeInt, NonNegativeInt, NonNegativeInt] = [255, 255, 255]
 
    
@@ -79,46 +85,39 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=5)
+app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=9)
 
 @app.post("/tiling")
 def tiling(params: Params):
-    writer = SVGWriter("out", size = (0, 0, 370, 220))
+    writer = SVGWriter("out", size = (0, 0, params.size[0], params.size[1]))
     writer.api_mode = True
-    if params.tess_id == "random":
-        tess_id = random.choice(list(js.keys()))
-    else:
-        tess_id = params.tess_id 
     writer.n_tiles = params.scale 
-    tess = js[tess_id]
-    if params.tess_type == "regular":
+    if params.tess_parameters.type == "regular":
+        if params.tess_parameters.tess_id == "random":
+            tess_id = random.choice(list(js.keys()))
+        else:
+            tess_id = params.tess_parameters.tess_id 
+        tess = js[tess_id]
         tesselation = RegularTesselation(writer, tess, tess_id)
-    elif params.tess_type == "hyperbolic":
-        if (params.n_sides - 2) * (params.n_neigh - 2) < 4:
+    elif params.tess_parameters.type == "hyperbolic":
+        if (params.tess_parameters.n_sides - 2) * (params.tess_parameters.n_neigh - 2) < 4:
             raise HTTPException(status_code=400, detail="Invalid parameters for hyperbolic tesselation. (n_neigh - 2) * (n_sides - 2) < 4.")
-        tesselation = HyperbolicTesselation(writer, params.n_sides, params.n_neigh, params.depth)
-        tesselation.half_plane = params.half_plane
-        tesselation.refine_tiling(params.refinements)
+        tesselation = HyperbolicTesselation(writer, 
+                                            params.tess_parameters.n_sides,
+                                            params.tess_parameters.n_neigh,
+                                            params.tess_parameters.depth)
+        tesselation.half_plane = params.tess_parameters.half_plane
+        tesselation.refine_tiling(params.tess_parameters.refinements)
     else:
-        tesselation = PenroseTesselation(writer, tile = TileType[params.tile])
+        tesselation = PenroseTesselation(writer, tile = TileType[params.tess_parameters.tile], level = params.tess_parameters.depth)
 
-    if (params.ornements):
-        writer.ornements = Ornements(type = params.ornements.type, 
-                                     width = params.ornements.width) 
+    writer.ornements = params.ornements
+    writer.hatching = params.hatching
     writer.color_line = params.color_line
     tesselation.set_angle(params.angle)
-    if params.hatching:
-        hatch_type = Hatching(
-            angle=params.hatching.angle,
-            spacing=params.hatching.spacing,
-            crosshatch=params.hatching.cross_hatch,
-            type=params.hatching.type,
-        )
-        writer.hatching = hatch_type
     if params.angle_parametrisation:
         tesselation.set_param_mode(params.angle_parametrisation)
     tesselation.writer = writer
-    t = time.time()
     svg = tesselation.draw_tesselation()
     
     return Response(svg)
